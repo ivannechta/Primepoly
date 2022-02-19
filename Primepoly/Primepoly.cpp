92bcb0f0191cff67fc9a977780e62f99e8330526
Ivan <ivannechta@gmail.com>
2022-02-19 15:47:59 +0700

    Init

---
+++ Primepoly/Primepoly.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "gtest/gtest.h"
+#include "Header.h"
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int main(int argc,char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);      
+    RUN_ALL_TESTS();    	        
+    //std::cout << "Hello World!\n";
+}
---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "..\Primepoly\Header.h"
+
+TEST(TestCaseName, TestName) {
+  //EXPECT_EQ(max(1,2), 2);
+  EXPECT_TRUE(true);
+}
