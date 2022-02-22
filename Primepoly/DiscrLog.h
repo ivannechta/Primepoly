662bd40c0c29093d40422c8ee5b50f7e9effa857
Ivan <ivannechta@gmail.com>
2022-02-22 20:27:00 +0700

    Найдены примитивные близницы.

--- Primepoly/DiscrLog.h
+++ Primepoly/DiscrLog.h
@@ -5,11 +5,18 @@ public:
 	Number Y;	
 public:	
 	void Divide(Number AX, Number P) {
+
+		if (AX.GetSize() < P.GetSize()){
+			Y = AX;
+			return;
+		}
+
 		Number tmp((char*)"1");
 		int i = 1;
 		while (tmp.GetSize() < P.GetSize()) {
 			tmp << AX[i++];
 		}
+		
 		while (i < AX.GetSize()) {
 
 			if (tmp.GetSize() < P.GetSize()) {
@@ -18,12 +25,17 @@ public:
 			else {
 				tmp = (tmp + P);
 				-tmp;
-			}
+			}			
 		}
+		
 		if (P < tmp) {
 			tmp = (tmp + P);
 			-tmp;
-		}	
+		}
+		if (P.GetSize() == tmp.GetSize()) {						
+			tmp = (tmp + P);
+			-tmp;			
+		}
 		Y = tmp;
 	}
 };
